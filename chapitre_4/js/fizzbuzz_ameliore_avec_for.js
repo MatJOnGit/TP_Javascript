2eabde516d0f73914146f2eb532a747ad67d2404
Mathieu Jourdan <ma.jourdan@hotmail.fr>
2018-03-21 15:39:11 +0100

    Edit notes in chapter 4 fizzbuzz_ameliore programs

--- chapitre_4/js/fizzbuzz_ameliore_avec_for.js
+++ chapitre_4/js/fizzbuzz_ameliore_avec_for.js
@@ -3,7 +3,7 @@ for (var i = 1; i <= 100; i++) {
         console.log("FizzBuzz");
     } else if ((i % 3 === 0) && (i % 5 != 0)) { // cas divisible par 3 et non divisible par 5 (tous les multiples de 3) => affiche "Fizz"
         console.log("Fizz");
-    } else if ((i % 3 != 0) && (i % 5 === 0)) { // cas divisible par 3 et non divisible par 5 (tous les multiples de 3) => affiche "Fizz"
+    } else if ((i % 3 != 0) && (i % 5 === 0)) { // cas divisible par 3 et non divisible par 5 (tous les multiples de 3) => affiche "Buzz"
         console.log("Buzz");
     } else { // tous les autres cas => affiche i
         console.log(i);
--- chapitre_4/js/fizzbuzz_ameliore_avec_while.js
+++ chapitre_4/js/fizzbuzz_ameliore_avec_while.js
@@ -4,7 +4,7 @@ while (i <= 100) {
         console.log("FizzBuzz");
     } else if ((i % 3 === 0) && (i % 5 != 0)) { // cas divisible par 3 et non divisible par 5 (tous les multiples de 3) => affiche "Fizz"
         console.log("Fizz");
-    } else if ((i % 3 != 0) && (i % 5 === 0)) { // cas divisible par 3 et non divisible par 5 (tous les multiples de 3) => affiche "Fizz"
+    } else if ((i % 3 != 0) && (i % 5 === 0)) { // cas divisible par 3 et non divisible par 5 (tous les multiples de 3) => affiche "Buzz"
         console.log("Buzz");
     } else { // tous les autres cas => affiche i
         console.log(i);
